382169547eece82bedf496742baede4b42bbcfa7
jgh33 <1035390779@qq.com>
2026-04-04 14:44:14 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "YFLog",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -17,8 +17,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
       .binaryTarget(name: "mars",
-                    url: "https://github.com/jgh33/YFLog/releases/download/0.0.2/mars.xcframework.zip",
-                    checksum: "97be17aac8cb31583c824807f87b766676a5fb8c3e0c630054cb338e303abc1d"
+                    url: "https://github.com/jgh33/YFLog/releases/download/0.0.3/mars.xcframework.zip",
+                    checksum: "148c14c9f843e731ef5aa6e5a3b998c48ba8ba8f0842fe3a3b3c1df2161e2d19"
       ),
 //      .binaryTarget(name: "mars",
 //                    path: "mars.xcframework"),
